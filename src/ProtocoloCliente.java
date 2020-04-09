ad6125b2474a69a1b8f5d24edf3cc823e8bca8b7
DaGalR <d.galindo@uniandes.edu.co>
2020-04-09 14:15:01 -0500

    Avance codigo

--- src/ProtocoloCliente.java
+++ src/ProtocoloCliente.java
@@ -5,15 +5,31 @@ import java.io.PrintWriter;
 public class ProtocoloCliente {
 	
 	public static void procesar(BufferedReader stdIn, BufferedReader pIn,PrintWriter pOut) throws IOException{
-		System.out.println("Escriba el mensaje para enviar: ");
-		String fromUser = stdIn.readLine();
 		
-		pOut.println(fromUser);
+		String resServidor, resCliente;
 		
-		String fromServer="";
-		
-		if((fromServer = pIn.readLine()) !=null) {
-			System.out.println("Respuesta del servidor: " + fromServer);
+		while(true) {
+			
+//			if(resServidor.equals("ERROR")) {
+//			
+//		}
+			resServidor = pIn.readLine();
+			System.out.println("Recibido: "+resServidor);
+			System.out.println("Escriba el mensaje a enviar al servidor: ");
+			resCliente=stdIn.readLine();
+			System.out.println();
+			
+			System.out.println("Escriba el mensaje para enviar: ");
+			String fromUser = stdIn.readLine();
+			
+			pOut.println(fromUser);
+			
+			String fromServer="";
+			
+			if((fromServer = pIn.readLine()) !=null) {
+				System.out.println("Respuesta del servidor: " + fromServer);
+			}
 		}
+	
 	}
 }
