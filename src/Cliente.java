ed01d65b900b09b75ea250a82eac2cadd264639f
Violeta Rodriguez <vl.rodriguez10@uniandes.edu.co>
2020-04-09 22:01:09 -0500

    Creo que cifrado listo

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,6 +1,10 @@
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -13,6 +17,54 @@ public class Cliente {
 		PrintWriter escritor = null;
 		BufferedReader lector = null;
 		System.out.println("Cliente...");
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+//		FileOutputStream archivoUsuarios = new FileOutputStream("docs/usuarios.txt");
+//		ObjectOutputStream escritorUsuarios = new ObjectOutputStream(archivoUsuarios);
+//		System.out.println("¿Desea ingresar o registrarse?");
+//		
+//		
+//		String tipoIngreso = stdIn.readLine();
+//		String  nombre = "";
+//		String usuario ="";
+//		String contrasenia="";
+//		
+//		if(tipoIngreso.equals("registrarse"))
+//		{
+//			System.out.println("¿Cuál es su nombre?");
+//			nombre = stdIn.readLine();
+//			
+//			System.out.println("¿Qué usuario le gustaría tener?");
+//			usuario = stdIn.readLine();
+//			
+//			//TO DO
+//			while(usuario.equals("prueba"))
+//			{
+//				System.out.println("Usuario no disponible, escoga otro usuario");
+//				usuario = stdIn.readLine();
+//			}
+//			
+//			System.out.println("¿Escriba una contraseña?");
+//			contrasenia= stdIn.readLine();
+//			
+//			System.out.println("Repita su contraseña");
+//			while(!contrasenia.contentEquals(stdIn.readLine()))
+//			{
+//				System.out.println("Las contraseñas no coinciden, escriba una nueva contraseña");
+//				contrasenia= stdIn.readLine();
+//				System.out.println("Repita su contraseña");
+//			}
+//			
+//			escritorUsuarios.writeChars(nombre);
+//			escritorUsuarios.writeChars(usuario);
+//			escritorUsuarios.writeChars(contrasenia);
+//			escritorUsuarios.close();
+//			
+//		}
+
+
+		
 		
 		try {
 			socket = new Socket(SERVIDOR,PUERTO);
@@ -24,7 +76,7 @@ public class Cliente {
 			System.exit(-1);
 		}
 		
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		
 		System.out.println("antes de procesarr");
 		ProtocoloCliente.procesar(stdIn,lector,escritor);
 		
@@ -33,4 +85,5 @@ public class Cliente {
 		lector.close();
 		socket.close();
 	}
+	
 }
