918fb14b1b6b06126bbc8457fba915bee485322a
DaGalR <d.galindo@uniandes.edu.co>
2020-04-09 22:48:57 -0500

    Avances pero no puedo probar

--- src/ProtocoloCliente.java
+++ src/ProtocoloCliente.java
@@ -1,11 +1,19 @@
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.security.Certificate;
+import java.security.InvalidKeyException;
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.SignatureException;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.util.Calendar;
 import javax.crypto.KeyGenerator;
@@ -25,8 +33,9 @@ public class ProtocoloCliente {
 	public static void procesar(BufferedReader stdIn, BufferedReader pIn,PrintWriter pOut) throws IOException{
 		
 		String[] algs;
-		String resServidor, resCliente;
-		resServidor ="";		
+		String resServidor, resCliente, stringSerServidor;
+		resServidor ="";
+		stringSerServidor="";
 		int contadorProtocolo = 0;
 		String algElegido="";
 		KeyPairGenerator generator;
@@ -35,16 +44,50 @@ public class ProtocoloCliente {
 		try {
 			generator = KeyPairGenerator.getInstance("RSA");
 			keyPair = generator.generateKeyPair();
-			
+			PublicKey kPubServ=null;
 			while(true) {
+				
+				
+
+				if(resServidor != null || contadorProtocolo!=0) {
+					System.out.println("RESPUESTA SERVIDOR " + resServidor);
+
+				}
+				
 				System.out.println("Contador va en " + contadorProtocolo);	
 				if(contadorProtocolo == 2 && algElegido != null && algElegido != "") {
-					System.out.println("entro if pro");
+					
 					try {
-						X509Certificate certificado = gc(keyPair,"SHA512");
+						
+						X509Certificate certificado = gc(keyPair,algElegido);
 						byte[] cerBytes = certificado.getEncoded();
 						String cerString = DatatypeConverter.printBase64Binary(cerBytes);
+						System.out.println("Certificado cliente " + cerString);
 						pOut.println(cerString);
+						contadorProtocolo++;
+						resServidor=pIn.readLine();
+						
+						if(resServidor.equals("OK")) {
+							
+							resServidor=pIn.readLine();
+							stringSerServidor = resServidor;
+							System.out.println("Certificado servidor " + stringSerServidor);
+							byte[] cerServByte = DatatypeConverter.parseBase64Binary(stringSerServidor);
+							CertificateFactory cf = CertificateFactory.getInstance("X.509");
+							X509Certificate c = (X509Certificate) cf.generateCertificate(new ByteArrayInputStream(cerServByte));
+							boolean valido = verificarCertificado(c);
+							if(valido) {
+								pOut.println("OK");
+								contadorProtocolo++;
+							}
+							else {
+								pOut.println("ERROR");
+
+							}
+							kPubServ = c.getPublicKey();
+							
+						}
+						continue;
 					} catch (OperatorCreationException | CertificateException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -55,14 +98,13 @@ public class ProtocoloCliente {
 				resCliente=stdIn.readLine();
 				
 				
-				
 				if(resCliente.equals("HOLA")) {
 					pOut.println(resCliente);
 				}
 				else if(resCliente.contains("ALGORITMOS:")) {
 					algs = resCliente.split(":");
 					if(algs.length == 4) {
-						algElegido = algs[3];
+						algElegido = algs[3].substring(4, algs[3].length());
 						System.out.println("ALGORITMO ELEGIDO " + algElegido);
 						resCliente.trim();
 						pOut.println(resCliente);
@@ -77,7 +119,6 @@ public class ProtocoloCliente {
 					continue;
 				}
 				resServidor = pIn.readLine();
-
 				if(resServidor.equals("ERROR")) {
 					System.out.println("Error detectado en servidor, intente escribir de nuevo su respuesta.");
 					continue;
@@ -104,6 +145,19 @@ public class ProtocoloCliente {
 		X509v3CertificateBuilder x509v3CertificateBuilder = new X509v3CertificateBuilder(new X500Name("CN=localhost"), BigInteger.valueOf(1), 		Calendar.getInstance().getTime(), endCalendar.getTime(), new X500Name("CN=localhost"), 		SubjectPublicKeyInfo.getInstance(keyPair.getPublic().getEncoded()));
 		ContentSigner contentSigner = new JcaContentSignerBuilder(alg+"withRSA").build(keyPair.getPrivate());
 		X509CertificateHolder x509CertificateHolder = x509v3CertificateBuilder.build(contentSigner);
-		return new JcaX509CertificateConverter().setProvider(new 		org.bouncycastle.jce.provider.BouncyCastleProvider()).getCertificate(x509CertificateHolder);
+		return new JcaX509CertificateConverter().setProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider()).getCertificate(x509CertificateHolder);
+	}
+	
+	public static boolean verificarCertificado(X509Certificate certificado) {
+		PublicKey llave = certificado.getPublicKey();
+		try {
+			certificado.verify(llave);
+			return true;
+		} catch (InvalidKeyException | CertificateException | NoSuchAlgorithmException | NoSuchProviderException
+				| SignatureException e) {
+			System.out.println("Error verificando certificado " + e.getMessage());
+			return false;
+
+		}
 	}
 }
